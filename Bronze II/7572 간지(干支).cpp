788d248738afc158b35dc20fd525d2459a5258fe
ul88 <hkimog13@naver.com>
2024-12-07 14:54:57 +0900

    reset

--- Bronze II/7572 간지(干支).cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int N;
-    scanf("%d",&N);
-    printf("%c%d\n",(N+8)%12+'A',(N+6)%10);
-    return 0;
-}
--- Bronze III/2525 오븐 시계.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int A,B,C;
-    cin>>A>>B>>C;
-    cout<<(A+(B+C)/60)%24<<" "<<(B+C)%60;
-    return 0;
-}
