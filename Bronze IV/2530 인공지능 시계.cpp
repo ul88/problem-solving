088eaa25aecf932af74d8d47fa76dfba5859fd88
ul88 <hkimog13@naver.com>
2024-12-07 22:34:56 +0900

    reset

--- Bronze IV/2530 인공지능 시계.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int A, B, C, D;
-    scanf("%d %d %d %d",&A,&B,&C,&D);
-
-    printf("%d %d %d",((((C+D) / 60 + B) / 60)+A)%24,((C+D) / 60 + B) % 60,(C+D) % 60); 
-    return 0;
-}
--- Silver III/2597 줄자접기.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    double n;
-    
-    return 0;
-}
